fc2b059d3375401ccfab80b04183b24bda3d1902
Hikmatillo2 <67962904+Hikmatillo2@users.noreply.github.com>
2022-08-26 16:34:22 +0700

    Update run.sh

--- djangoProject5/run.sh
+++ djangoProject5/run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 service nginx start
-/srv/djangoProject5/migrate.sh
+# service posgresql start
+/bin/python3 awaiter.py
+if [ ! -f "/srv/djangoProject5/flag" ]; then
+    python3 manage.py collectstatic --noinput --settings settings.development
+    python3 manage.py migrate --settings settings.development && sleep 1
+    python3 manage.py makemigrations --settings settings.development && sleep 1
+    python3 manage.py migrate --settings settings.development && sleep 1
+    python3 manage.py initadmin --settings settings.development
+    echo 1 > /srv/djangoProject5/flag
+fi
+
 /bin/gunicorn3 wsgi:application -b 127.0.0.1:8000 --env DJANGO_SETTINGS_MODULE=settings.development --user www-data --group www-data

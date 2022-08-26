94e0c1f33f7d8d82c102d692b9d58e66de010426
Hikmatillo2 <67962904+Hikmatillo2@users.noreply.github.com>
2022-08-26 15:23:38 +0700

    Create migrate.sh

---
+++ djangoProject5/migrate.sh
@@ -0,0 +1,6 @@
+python3 manage.py collectstatic --noinput --settings settings.development
+python3 manage.py migrate --settings settings.development && \
+sleep 1 && python3 manage.py makemigrations --settings settings.development && sleep 1 && \
+python3 manage.py migrate --settings settings.development && sleep 1 && \
+python3 manage.py initadmin --settings settings.development
+echo "" > /srv/djangoProject5/migrate.sh

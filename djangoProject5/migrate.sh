58ade645246b342b43a7714a9b49623fdcf6db2a
Hikmatillo2 <67962904+Hikmatillo2@users.noreply.github.com>
2022-08-26 16:38:14 +0700

    Delete migrate.sh

--- djangoProject5/migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 manage.py collectstatic --noinput --settings settings.development
-python3 manage.py migrate --settings settings.development && \
-sleep 1 && python3 manage.py makemigrations --settings settings.development && sleep 1 && \
-python3 manage.py migrate --settings settings.development && sleep 1 && \
-python3 manage.py initadmin --settings settings.development
-echo "" > /srv/djangoProject5/migrate.sh

a78f3dcc9975686d138748a7496b7aa1c94504cd
Hikmatillo2 <67962904+Hikmatillo2@users.noreply.github.com>
2022-08-26 13:48:32 +0700

    Update run.sh

--- djangoProject5/run.sh
+++ djangoProject5/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 service nginx start
+service posgresql start
 /bin/gunicorn3 wsgi:application -b 127.0.0.1:8000 --env DJANGO_SETTINGS_MODULE=settings.development --user www-data --group www-data

228a9cad7e5f0f6e71a74f6e1725f2fcafe3279c
Hikmatillo2 <67962904+Hikmatillo2@users.noreply.github.com>
2022-08-26 16:12:45 +0700

    Update run.sh

--- djangoProject5/run.sh
+++ djangoProject5/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-'''service nginx start
+service nginx start
 /srv/djangoProject5/migrate.sh
-/bin/gunicorn3 wsgi:application -b 127.0.0.1:8000 --env DJANGO_SETTINGS_MODULE=settings.development --user www-data --group www-data'''
+/bin/gunicorn3 wsgi:application -b 127.0.0.1:8000 --env DJANGO_SETTINGS_MODULE=settings.development --user www-data --group www-data

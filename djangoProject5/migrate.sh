03d24c1a4566dcba8e4be3bb24b74c3ae74697d5
Hikmatillo2 <67962904+Hikmatillo2@users.noreply.github.com>
2022-08-26 15:30:11 +0700

    Update migrate.sh

--- djangoProject5/migrate.sh
+++ djangoProject5/migrate.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 python3 manage.py collectstatic --noinput --settings settings.development
 python3 manage.py migrate --settings settings.development && \
 sleep 1 && python3 manage.py makemigrations --settings settings.development && sleep 1 && \

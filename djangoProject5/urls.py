cf8a4947c2572255b1ff962edad08ee22c7ced9b
Hikmatillo2 <67962904+Hikmatillo2@users.noreply.github.com>
2022-09-04 00:34:37 +0700

    Update urls.py

--- djangoProject5/urls.py
+++ djangoProject5/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from views import *
-
+'''
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(f'{development.TOKEN}', get_message),
-]
+]'''
